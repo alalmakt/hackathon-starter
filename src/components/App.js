2e479405d78b00c5bb5b8d9b05ce87dd823fc896
Ali <ali.almaktoum@gmail.com>
2017-12-01 12:26:39 +1000

    Added kendo

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React from "react";
 import "../stylesheets/main.scss";
 
+import * as kendo from '@progress/kendo-ui';
+/* Alternative approaches to load Kendo UI will be:
+import '@progress/kendo-ui';
+require("@progress/kendo-ui/js/kendo.all.js"); */
+import '@progress/kendo-theme-default/dist/all.css';
+import { Calendar } from '@progress/kendo-dateinputs-react-wrapper';
+
 // app component
 export default class App extends React.Component {
-  // render
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        dateTime: new Date()
+    };
+
+    this.onchange = this.onchange.bind(this);
+}
+
+onchange(e) {
+    this.setState({
+        dateTime: e.sender.value()
+    });
+}
   render() {
     return (
       <div className="container">
         {this.props.children}
+        <Calendar value={this.state.dateTime} change={this.onchange}/>
       </div>
     );
   }
